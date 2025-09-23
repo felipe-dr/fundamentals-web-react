d893d1fe22888672b0014e2b8962479584aa1da2
Felipe DR <felipe.corp7@gmail.com>
2025-09-23 13:14:44 -0300

    chore: change to single quote

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Header } from "./components/Header";
-import { Post } from "./Post";
+import { Header } from './components/Header';
+import { Post } from './Post';
 
-import "./global.css";
+import './global.css';
 
 export function App() {
   return (
