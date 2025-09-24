1022c488eae3dd4d8788dea68e79a941b618b537
Felipe DR <felipe.corp7@gmail.com>
2025-09-24 11:18:38 -0300

    feat: class-01 comments form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -10,7 +10,7 @@ export function Post() {
             src="https://github.com/felipe-dr.png"
           />
           <div className={styles.authorInfo}>
-            <strong>Diego Fernandes</strong>
+            <strong>John Doe</strong>
             <span>Web Developer</span>
           </div>
         </div>
@@ -34,6 +34,14 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
