1d05b7cbad71bd83a50e44fc91f93c7bcbb8ce7b
Felipe DR <felipe.corp7@gmail.com>
2025-09-24 11:31:40 -0300

    feat: class-01 comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import styles from './Comment.module.css';
+
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/felipe-dr.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>John Doe</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
